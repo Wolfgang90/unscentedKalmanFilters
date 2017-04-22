060eda5b5478772016dbfedc5e1d8763bcf8c24b
Wolfgang90 <eichler_wolfgang@msn.com>
2017-04-22 16:09:47 +0200

    RMSE-calculation implemented

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "tools.h"
+#include "math.h"
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
@@ -15,4 +16,27 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  /**
+    * RMSE-Formula: RMSE = sqrt(1/n * sum[from 1 to n](x_est - x_true)**2) 
+  */
+
+  assert(estimations.size() == ground_truth.size() || estimations.size() == 0 && "Estimation and ground truth vector do not have the same size");
+
+  //initialized required values for formula
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+  double n = estimations.size();
+  
+  for (int i = 0; i < n; i++) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+
+    //coefficient-wise multiplication
+    residual = residual.array()*residual.array();
+    rmse += residual;
+    }
+
+  rmse = (rmse / n);
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
